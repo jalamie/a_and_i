bdadf52a0b2cedce1b86d42175d2be8ecccc2e48
jalamie <121021507+jalamie@users.noreply.github.com>
2025-04-17 09:26:26 +0800

    Update App.js
    remove firebase config

--- my-app/App.js
+++ my-app/App.js
@@ -12,13 +12,7 @@ import GateDetail from './components/GateDetail';
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDc3qxRx6i28a_tY3bMB0tXWK3jM7MUo-g",
-  authDomain: "capstone-sal.firebaseapp.com",
-  projectId: "capstone-sal",
-  storageBucket: "capstone-sal.firebasestorage.app",
-  messagingSenderId: "506687469413",
-  appId: "1:506687469413:web:661076c64cf63e6197138c",
-  measurementId: "G-YPD0ZE5W55"
+  //place firebaseConfig here
 };
 
 // Initialize Firebase
